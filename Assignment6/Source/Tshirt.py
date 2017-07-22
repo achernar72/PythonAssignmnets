70d0f5c82681b6a4c399672fddff063ff2f4c2f2
achernar72 <sankarasettyavinash@gmail.com>
2017-07-22 15:15:35 -0500

    the screen sots are addd

--- Assignment6/Source/Tshirt.py
+++ Assignment6/Source/Tshirt.py
@@ -1,15 +1,18 @@
 import numpy as np
 from matplotlib import pyplot as plt
 import cv2
+import sys
+
+sys.path.append('/usr/local/lib/python2.7/site-packages')
 
 X = np.random.randint(25,50,(25,2))
 Y = np.random.randint(60,85,(25,2))
 Z = np.vstack((X,Y))
 
-# convert to np.float32
+#now we have to convert to np.float32
 Z = np.float32(Z)
 
-# define criteria and  kmeans()
+# now we have to define criteria and  kmeans()
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
 ret,label,center=cv2.kmeans(Z,2,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
 
@@ -17,7 +20,7 @@ ret,label,center=cv2.kmeans(Z,2,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
 A = Z[label.ravel()==0]
 B = Z[label.ravel()==1]
 
-# Plot the data
+# Plot the graph from the given data so we have to use the height and weight variables 
 plt.scatter(A[:,0],A[:,1])
 plt.scatter(B[:,0],B[:,1],c = 'r')
 plt.scatter(center[:,0],center[:,1],s = 80,c = 'y', marker = 's')
